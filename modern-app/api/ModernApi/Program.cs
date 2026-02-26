ca2deb7e8c39a8b396dccc8aa241a9cc120337f2
Badhusha Arabusha <C8Q9HT@glbcore.com>
2026-02-26 21:48:06 +0530

    Migrated Quotes Module

--- modern-app/api/ModernApi/Program.cs
+++ modern-app/api/ModernApi/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<ModernApi.Services.QuoteService>();
 
 var app = builder.Build();
 
